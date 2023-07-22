35e83d5ca76d65557c840272dada913453c13957
Konstantin <KonstantKar@yandex.ru>
2023-07-22 15:47:19 +0300

    Стилизован About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,49 @@
 import React from "react";
-import "./About.module.css";
+import "./About.css";
+import ME from "../../assets/yong.jpg";
+import { FaAward } from "react-icons/fa";
+import { PiProjectorScreenChartLight } from "react-icons/pi";
 const About = () => {
-  return <section id="about">About </section>;
+  return (
+    <section id="about">
+      <h5>А ТЕПЕРЬ!</h5>
+      <h2>Немного обо мне</h2>
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <div className="about__icon">
+                <FaAward />
+              </div>
+              <h2>Опыт</h2>
+              <small>Год самостоятельного обучения</small>
+            </article>
+            <article className="about__card">
+              <div className="about__icon">
+                <PiProjectorScreenChartLight />
+              </div>
+              <h2>Проекты</h2>
+              <small>Их немного, но я ими горжусь!</small>
+            </article>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam
+            atque consequatur facere eligendi voluptate ipsam! Tenetur,
+            possimus? Aliquid accusantium amet, commodi vel debitis eligendi
+            doloremque aperiam et quam rem vitae.
+            <a href="contact" className="btn btn-primary">
+              Напишите мне!
+            </a>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
