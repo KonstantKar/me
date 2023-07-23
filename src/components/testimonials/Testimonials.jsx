838e795c0e47deca3e6bf78f272e51c7643f8ed3
Konstantin <KonstantKar@yandex.ru>
2023-07-23 16:47:26 +0300

    В Testimonials добавлен Slider

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,24 +1,71 @@
 import React from "react";
 import "./Testimonials.css";
 import Anton from "../../assets/Anton.jpg";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+import { PiArrowFatLinesRight } from "react-icons/pi";
+// Import Swiper styles
+import "swiper/css";
 const Testimonials = () => {
   return (
     <section id="testimonials">
       <h5>Отзывы</h5>
       <h2>Честные-честные</h2>
-      <div className="container testimonials__container">
-        <article className="testimonials__review">
-          <div className="testimonials__avatar">
-            <img src={Anton} alt="" />
+      <Swiper
+        className="container testimonials__container"
+        modules={(Pagination, Navigation)}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide>
+          <div className="testimonials__review">
+            <div className="testimonials__avatar">
+              <img src={Anton} alt="" />
+            </div>
+            <h4>Антон Караманов</h4>
+            <h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+              consequuntur ex blanditiis consectetur reiciendis vitae aspernatur
+              suscipit dsdsdsdstias quia optio.
+            </h3>
+            <h5>
+              Пролистай дальше! <PiArrowFatLinesRight />
+            </h5>
           </div>
-          <h4>Антон Караманов</h4>
-          <h3>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-            consequuntur ex blanditiis consectetur reiciendis vitae aspernatur
-            suscipit molestias quia optio.
-          </h3>
-        </article>
-      </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="testimonials__review">
+            <div className="testimonials__avatar">
+              <img src={Anton} alt="" />
+            </div>
+            <h4>Антон Караманов</h4>
+            <h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+              consequuntur ex blanditiis consectetur reiciendis vitae aspernatur
+              suscipit molestias quia optio.
+            </h3>
+            <h5>
+              Пролистай дальше! <PiArrowFatLinesRight />
+            </h5>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="testimonials__review">
+            <div className="testimonials__avatar">
+              <img src={Anton} alt="" />
+            </div>
+            <h4>Антон Караманов</h4>
+            <h3>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+              consequuntur ex blanditiis consectetur reiciendis vitae aspernatur
+              suscipit molestias quia optio.
+            </h3>
+            <h5>Конец отзывов!</h5>
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </section>
   );
 };
